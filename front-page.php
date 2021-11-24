4be6ef5b2f092e2bda36edade582e244ae7c7814
Erik <savstromerik@gmail.com>
2021-11-24 13:44:50 +0100

    front page fix

---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php
+    get_header(); 
+?>
+<div class="container">
+    <div class="row g-4">
+    <div class="container">
+        <div class="row" style="gap: 0.5em;">
+            <div class="col card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="col card" style="width: 18rem;">
+            <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="col card" style="width: 18rem;">
+            <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+        </div>
+    </div>
+
+    <div> 
+        <h1 class="p-3">Some text about the company</h1>
+        <p class="text-wrap p-3" style="width: 30em;">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem laborum,
+             repudiandae fuga blanditiis repellendus aut? Labore cum, aperiam quaerat veritatis doloribus,
+             nulla accusantium autem iste facere, quo eligendi laboriosam sunt?
+        </p>
+        </div>
+    </div>
+    </div>
+ </div>
+
+<?php get_footer(); ?>
