dc18e23289c12fafaa2dde3d4d22b9da25a3589a
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-11-24 14:05:16 +0100

    register menu in functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-function add_theme_scripts(): void {
+function add_theme_scripts(): void
+{
     wp_enqueue_style("bootstrap", get_template_directory_uri() . "/bootstrap/css/bootstrap.min.css");
     wp_enqueue_script("bootstrap", get_template_directory_uri() . "/bootstrap/js/bootstrap.bundle.min.js");
     wp_enqueue_style("style", get_stylesheet_uri());
@@ -8,3 +9,20 @@ function add_theme_scripts(): void {
 }
 
 add_action("wp_enqueue_scripts", "add_theme_scripts");
+
+if (!function_exists('themeSetup')) :
+
+    function themeSetup()
+    {
+        add_theme_support('menus');
+        add_theme_support('post-thumbnails');
+        register_nav_menus(
+            array(
+                'primary-menu' => 'Main menu location',
+
+            )
+        );
+    }
+
+endif;
+add_action('after_setup_theme', 'themeSetup');
