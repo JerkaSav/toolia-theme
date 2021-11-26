c00e0a4fdadc80ad3fc5f0ca3854670d0c32ae1d
Thomas Laukkanen <tumppu91@hotmail.com>
2021-11-26 14:33:09 +0100

    new styles

--- template-cases.php
+++ template-cases.php
@@ -3,16 +3,26 @@
 Template Name: Cases
 */
 get_header();
+$title = get_field('casetitel');
+$img = get_field('case_img')
 ?>
 
 
 <section class="container">
 
-  <h1 class="display-2 text-center mt-5">
-    <?php the_title() ?>
-  </h1>
+  <?php if ($title) : ?>
 
-  <p class="mb-8"><?php get_template_part('/includes/sections', 'content'); ?></p>
+    <h1 class="display-2 text-center mt-5"><?php echo $title; ?></h1>
+  <?php else : ?>
+    <h1 class="display-2 text-center mt-5">Uppdrag vi har gjort</h1>
+  <?php endif; ?>
+
+
+
+  <div class="mx-auto" style="max-width: 40rem;">
+
+    <p class="mb-5"><?php get_template_part('/includes/sections', 'content'); ?></p>
+  </div>
 
 
   <?php
@@ -22,21 +32,17 @@ get_header();
   <?php if ($the_query->have_posts()) : ?>
 
 
-
-
-
     <!-- the loop -->
 
-    <div class="row justify-content-around mt-6">
+    <div class="row justify-content-between my-5">
       <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
+        <div class="card shadow-lg border-0" style="width: 18rem;">
+          <div class="card-body ">
             <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
             <h5 class="card-title"><?php the_title(); ?> </h5>
-
             <p class="card-text"><?php the_excerpt(); ?></p>
-            <a href="<?php echo the_permalink(); ?>" class="card-link"><?php the_title(); ?></a>
+            <a href="<?php echo the_permalink(); ?>" class="card-link">Läs mer</a>
           </div>
         </div>
 
@@ -53,6 +59,20 @@ get_header();
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
   <?php endif; ?>
 
+  <div class="card mb-3 mx-auto shadow-lg border-0" style="">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="<?php echo $img['sizes']['large']; ?>" class="img-fluid rounded-start" alt="">
+      </div>
+      <div class="col-md-8 my-auto p-4">
+        <div class="card-body ">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer".</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php get_footer(); ?>
