516d41acb53c09c813600f16e4981548d6658c65
Thomas Laukkanen <tumppu91@hotmail.com>
2021-11-30 10:03:54 +0100

    thomas

--- template-cases.php
+++ template-cases.php
@@ -4,8 +4,12 @@ Template Name: Cases
 */
 get_header();
 $title = get_field('casetitel');
-$img = get_field('case_img')
+$img = get_field('case_img');
+$case_titel = get_field('case_titel');
+$text = get_field('case_text');
+$case_author = get_field('case_author');
 ?>
+
 
 
 <section class="container">
@@ -36,7 +40,6 @@ $img = get_field('case_img')
 
     <div class="row justify-content-between my-5">
       <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
         <div class="card shadow-lg border-0" style="width: 18rem;">
           <div class="card-body ">
             <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
@@ -64,12 +67,14 @@ $img = get_field('case_img')
       <div class="col-md-4">
         <img src="<?php echo $img['sizes']['large']; ?>" class="img-fluid rounded-start" alt="">
       </div>
-      <div class="col-md-8 my-auto p-4">
+      <div class="col-md-8 p-4 my-auto">
         <div class="card-body ">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer".</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <h5 class="card-title"><?php echo $case_titel ?></h5>
+          <p class="card-text">"<?php echo $text ?>"</p>
+          <p class="card-text"><small class="text-muted"><?php echo $case_author ?></small></p>
+
         </div>
+        <div class="card-footer bg-transparent shadow-sm border-0 color-succes">Ökad försäljning - Google Ads - SEO - Wordpress</div>
       </div>
     </div>
   </div>
