3f3c8ac9209162275ab731516a694d032ea8fb82
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-12-02 18:11:06 +0100

    add archive-job.php and some styling * added custom fields for post-type 'job'

---
+++ archive-job.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div class="container-md">
+    <h1 class="fp-h-b ps-sm-5 pt-sm-4 pb-sm-4 my-5">Available positions</h1>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="post-excerpt card mb-3">
+                <div class="card-body">
+                    <h1 class="card-title blog-post-title"><a href="<?php echo the_permalink(); ?>"><?php the_title() ?> </a></h1>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                        </svg>
+                        <?php echo the_field('location', get_the_ID())   ?>
+                    </p>
+
+                </div>
+            </div>
+
+        <?php endwhile;
+    else : ?>
+        <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+
+    <?php endif; ?>
+</div>
+
+<?php get_footer() ?>
